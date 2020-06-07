457e644fa434a497f6dd9c6ba169271cff6b3700
juikiran <59646096+juikiran@users.noreply.github.com>
2020-06-07 18:06:19 +0530

    Update stringconcat.js

--- js/stringconcat.js
+++ js/stringconcat.js
@@ -3,4 +3,5 @@ var message2 ="Welcome to Web Developer Class."
 
 var stuName = prompt("User ID:");
 var space = " ";
-document.write (message1 + space + stuName + "! " + message2);
+/*document.write (message1 + space + stuName + "! " + message2);*/
+document.getElementById("test").innerHTML = message1 + space + stuName + "! " + message2;
